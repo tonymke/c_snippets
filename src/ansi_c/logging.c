70a7d0925c0333d03b306539458a879d0712311c
Tony Lechner <tony@tony-lechner.com>
2025-03-20 23:57:47 -0500

    Simplify logging

--- src/ansi_c/logging.c
+++ src/ansi_c/logging.c
@@ -1,4 +1,6 @@
 #include <assert.h>
+#include <ctype.h>
+#include <math.h>
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -15,20 +17,16 @@ static enum log_level current_lvl = LOGGING_LEVEL_INITIAL;
 
 static int is_valid_log_level(enum log_level lvl);
 static const char *log_level_s(enum log_level lvl);
-static size_t digit_count(unsigned n);
+static char *rstrip(char *s);
 
 int log_emit(enum log_level lvl, const char *file, unsigned lineno,
 	     const char *fmt, ...)
 {
-#ifndef LOGGING_LOG_METADATA_BUF_CAP
-#define LOGGING_LOG_METADATA_BUF_CAP 512U
-#endif
-	static char metadata_buf[LOGGING_LOG_METADATA_BUF_CAP];
-	const char *ctime_s, *lvl_s;
+	int written = -1;
 	va_list ap;
 	time_t now;
-	size_t needed_len, tot_needed_len = 2; /* terminating linebreak + \0 */
-	int written;
+	char *now_s;
+	const char *lvl_s;
 
 	assert(is_valid_log_level(lvl));
 	assert(file != NULL);
@@ -38,94 +36,38 @@ int log_emit(enum log_level lvl, const char *file, unsigned lineno,
 		return 0;
 	}
 
-	/*
-	   Stderr is typically unbuffered, and interleaved log lines are not
-	   very useful. As well, we want to provide a way for the use to
-	   leverage printf. So, we can't easily just do a single vprintf call
-	   and be done with it.
-
-	   Instead, pre-write the log's metadata/attrs and the user's fmt string
-	   into a buffer, so we can then call vfprintf against their fmt string.
-	   
-	   C90 does not have snprintf(3). To prevent an overflow, we therefore
-	   need to pre-calculate how much metadata_buf space will be used before
-	   calling sprintf.
-	*/
-
-	/* ctime string + wrapping brackets */
 	now = time(NULL);
-	needed_len = 2;
 	if (now == (time_t)-1) {
-		ctime_s = "";
-	} else {
-		ctime_s = ctime(&now);
-		if (ctime_s == NULL) {
-			ctime_s = "";
-		} else {
-			/* remove the suffixing newline, infer the strlen from
-			 * its address if possible */
-			char *newline = strchr(ctime_s, '\n');
-			if (newline != NULL) {
-				*newline = '\0';
-				needed_len += (newline - ctime_s);
-			} else {
-				needed_len += strlen(ctime_s);
-			}
-		}
+		return -1;
 	}
-	assert((size_t)-1 - tot_needed_len >= needed_len);
-	tot_needed_len += needed_len;
-
-	/* level string + prefixing space */
-	lvl_s = log_level_s(lvl);
-	assert(lvl_s != NULL);
-	needed_len = 1 + strlen(lvl_s);
-	assert((size_t)-1 - tot_needed_len >= needed_len);
-	tot_needed_len += needed_len;
-
-	/* file string + prefixing space */
-	needed_len = 1 + strlen(file);
-	assert((size_t)-1 - tot_needed_len >= needed_len);
-	tot_needed_len += needed_len;
-
-	/* lineno string + prefixing colon */
-	needed_len = 1 + digit_count(lineno);
-	assert((size_t)-1 - tot_needed_len >= needed_len);
-	tot_needed_len += needed_len;
-
-	/* fmt + prefixing colon + suffixing newline */
-	needed_len = strlen(fmt);
-	assert((size_t)-1 - 2 > needed_len);
-	needed_len += 2;
-	assert((size_t)-1 - tot_needed_len >= needed_len);
-	tot_needed_len += needed_len;
-
-	if (tot_needed_len > sizeof(metadata_buf) - 1) {
-		assert(!"log fmt string too long");
+	now_s = rstrip(ctime(&now));
+	if (now_s == NULL) {
 		return -1;
 	}
 
-	/* write the metadata buf.
-	   this becomes "(metadata) (user_fmt_str)\n" */
-	written = sprintf(metadata_buf,
+	lvl_s = log_level_s(lvl);
+	assert(lvl_s != NULL);
+
+	written = fprintf(stderr,
 			  ("[%s]" /* time */
-			   " %s" /* lvl */
-			   " %s:%d" /* file:lineno */
-			   " %s\n"), /* fmt */
-			  ctime_s, lvl_s, file, lineno, fmt);
+			   " "
+			   "%s" /* level */
+			   " "
+			   "%s:%d" /* file:lineno */
+			   " "),
+			  now_s, lvl_s, file, lineno);
 	if (written < 0) {
 		return -1;
 	}
-	assert((unsigned)written < sizeof(metadata_buf) - 1);
 
-	/*
-	Now, use it to printf with the provided fmt str and args all in one
-	printf call. If this string was less than PIPE_BUF (512byte min in
-	POSIX, 4k on linux) we don't have to care about interleaving at all)
-	*/
 	va_start(ap, fmt);
-	written = vfprintf(stderr, metadata_buf, ap);
+	written = vfprintf(stderr, fmt, ap);
 	va_end(ap);
+	if (written < 0) {
+		return -1;
+	}
+
+	written = fprintf(stderr, "\n");
 	if (written < 0) {
 		return -1;
 	}
@@ -177,18 +119,31 @@ static const char *log_level_s(enum log_level lvl)
 	return level_ss[lvl];
 }
 
-static size_t digit_count(unsigned n)
+static char *rstrip(char *s)
 {
-	size_t count = 0;
+	char *cur, *ws_start;
 
-	if (n == 0) {
-		return 1;
+	if (s == NULL) {
+		return NULL;
 	}
 
-	while (n != 0) {
-		n /= 10;
-		count++;
+	ws_start = NULL;
+	cur = s;
+	while (*cur != '\0') {
+		if (isspace(*cur)) {
+			if (ws_start == NULL) {
+				ws_start = cur;
+			}
+		} else {
+			ws_start = NULL;
+		}
+		cur++;
 	}
 
-	return count;
+	if (ws_start != NULL) {
+		*ws_start = '\0';
+		cur = ws_start;
+	}
+
+	return s;
 }
